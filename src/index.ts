0d0114ec9bfd3bb7f56d7d3a2442e2c8db2cfca7
DATBOI <alexsimons9999@gmail.com>
2020-03-01 17:35:15 -0600

    Debating whether or not this should be fancy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
+import { debug } from "console"
 
-export const decorateConfig = (config: any) => {
-  console.log('hello world');
-  
+export {reduceSessions, reduceUI} from './store/reducers';
+
+
+const DOKI_THEME_COMMAND = 'doki-theme';
+const isDokiThemeCommand = (
+  command: string,
+  onCommand: (theme: string) => void) => {
+  const dokiCommandIndex = command.indexOf(DOKI_THEME_COMMAND);
+  const isDokiCommand = dokiCommandIndex > -1;
+  if (isDokiCommand) {
+    const newLocal = command.substr(dokiCommandIndex + DOKI_THEME_COMMAND.length).trim();
+    debug(`command is: "${newLocal}"`);
+    onCommand(newLocal)
+  }
+}
+
+export const middleware = (store: any) =>
+  (next: any) => (action: any) => {
+    if ('SESSION_ADD_DATA' === action.type &&
+      action.data && action.data.charCodeAt(0) === 13) {
+      debug(JSON.stringify(store, null, 2))
+      const { data } = action;
+      // isDokiThemeCommand(data,
+      // dokiTheme => store.dispatch({
+      // type: 'DOKI_THEME_SET',
+      // data: dokiTheme,
+      // }));
+    }
+    next(action)
+  }
+
+
+
+const decorateConfig = (config: any) => {
+  return {
+    config: {
+    },
+    syntax: {
+
+    },
+    termCSS: '',
+    css: ''
+  }
 }
