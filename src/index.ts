8f116cb64e5ea64f8041818c3769a23d81e5ce16
DATBOI <alexsimons9999@gmail.com>
2020-03-06 04:12:22 -0600

    Okay, let's not do any of that

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,5 @@
 import { debug } from "console"
 
-export {reduceSessions, reduceUI} from './store/reducers';
-
-
-const DOKI_THEME_COMMAND = 'doki-theme';
-const isDokiThemeCommand = (
-  command: string,
-  onCommand: (theme: string) => void) => {
-  const dokiCommandIndex = command.indexOf(DOKI_THEME_COMMAND);
-  const isDokiCommand = dokiCommandIndex > -1;
-  if (isDokiCommand) {
-    const newLocal = command.substr(dokiCommandIndex + DOKI_THEME_COMMAND.length).trim();
-    debug(`command is: "${newLocal}"`);
-    onCommand(newLocal)
-  }
-}
-
-export const middleware = (store: any) =>
-  (next: any) => (action: any) => {
-    if ('SESSION_ADD_DATA' === action.type &&
-      action.data && action.data.charCodeAt(0) === 13) {
-      debug(JSON.stringify(store, null, 2))
-      const { data } = action;
-      // isDokiThemeCommand(data,
-      // dokiTheme => store.dispatch({
-      // type: 'DOKI_THEME_SET',
-      // data: dokiTheme,
-      // }));
-    }
-    next(action)
-  }
-
-
-
 const decorateConfig = (config: any) => {
   return {
     config: {
--- src/store/actions.ts
+++
@@ -1,8 +0,0 @@
-export const CONFIG_LOAD = 'CONFIG_LOAD';
-export const CONFIG_RELOAD = 'CONFIG_RELOAD';
-export const SESSION_USER_DATA = 'SESSION_USER_DATA';
-export const SESSION_ADD = 'SESSION_ADD';
-
-export const PLUGIN = 'doki-theme';
-export const INPUT_STORAGE_PATH = `${PLUGIN}/userInputs`
-export const UI_DATA_PATH = 'uiData';
