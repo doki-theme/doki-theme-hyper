0854715667e2649722a9dcf7659912601d66603d
Alex Simons <alexsimons9999@gmail.com>
2021-02-15 09:09:42 -0600

    v8 [Zero Two & Sakurajima Mai] (#32)

--- src/settings.ts
+++ src/settings.ts
@@ -61,7 +61,7 @@ const themes = Object.values(DokiThemeDefinitions)
     };
   });
 
-export const VERSION = "v7.1.0";
+export const VERSION = "v8.0.0";
 const icon = path.resolve(__dirname, "..", "assets", "Doki-Theme.png");
 const showAbout = () => {
   const appName = "Doki Theme";
